c21e8e99489efca4a074a7c780b498b30d46d1f1
Mikhail <mmishkarudny@neklo.com>
2023-01-29 14:31:01 +0300

    HW 5 Lesson

---
+++ Lesson-5/lessonFile5.js
@@ -0,0 +1,27 @@
+let newString = 'true';
+let newBool = false;
+let newNumber = 10;
+let newNumberStr = '20;'
+
+console.log(newString + newBool);
+console.log(newString + newNumber);
+console.log(newNumber + newBool);
+
+console.log(newString * newBool);
+console.log(newString * newNumber);
+console.log(newNumber * newBool);
+
+console.log(newString / newBool);
+console.log(newString / newNumber);
+console.log(newNumber / newBool);
+
+console.log(parseInt(newNumberStr)); 
+console.log(newBool.toString());
+console.log(Boolean(newString));
+
+
+
+
+
+
+
